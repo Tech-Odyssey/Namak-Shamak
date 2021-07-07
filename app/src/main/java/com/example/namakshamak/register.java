4d060a1ce418fb9ba3c7284804e58ff2af6efba8
Arnavt14 <77206185+Arnavt14@users.noreply.github.com>
2021-07-07 09:05:42 +0530

    authentication

--- app/src/main/java/com/example/namakshamak/register.java
+++ app/src/main/java/com/example/namakshamak/register.java
@@ -1,14 +1,71 @@
 package com.example.namakshamak;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class register extends AppCompatActivity {
+
+    EditText mName,mEmail,mPass;
+    Button mRegister;
+    FirebaseAuth auth;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        mName = findViewById(R.id.editTextTextPersonName2);
+        mEmail = findViewById(R.id.editTextTextPersonName3);
+        mPass = findViewById(R.id.editTextTextPassword3);
+
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser() !=null){
+            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            finish();
+        }
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString().trim();
+                String pass = mPass.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email)){
+                    mEmail.setError("Email is required");
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    mPass.setError("Password is required");
+                    return;
+                }
+
+                auth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(register.this, "Registration Successful", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                        }else{
+                            Toast.makeText(register.this, "Error! " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
+
     }
 }
