4b8bf831509166d9b43fe4af3094b0e56229ebbc
Parijat Rai <parijatrai02@gmail.com>
2021-07-08 21:10:37 +0530

    Color added to navigation and status bars

--- app/src/main/java/com/example/namakshamak/MainActivity.java
+++ app/src/main/java/com/example/namakshamak/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.namakshamak;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 
